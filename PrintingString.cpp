bf8f21c07675ae40a22e5ce999f3f73d364e3ec7
ayushkumar7804 <ayushkumaryadav2029@gmail.com>
2025-08-06 00:14:58 +0530

    Create PrintingString.cpp

---
+++ PrintingString.cpp
@@ -0,0 +1,18 @@
+// Ayush Yadav
+// 24070123028
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str1;
+    cout<<"Enter a string\n";
+    cin>>str1;
+    cout<<"Entered string is "<<str1;
+    return 0;
+}
+
+// OUTPUT
+// Enter a string
+// Ayush
+// Entered string is Ayush 
