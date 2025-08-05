f26e8180bdf3dffe6054d3ae0538f52ecc686f93
ayushkumar7804 <ayushkumaryadav2029@gmail.com>
2025-08-05 23:57:41 +0530

    Update concatenation of strings.cpp

--- concatenation of strings.cpp
+++ concatenation of strings.cpp
@@ -1,1 +1,26 @@
+// Ayush Yadav
+// 24070123028
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str1;
+    string str2;
+    string str3;
+    
+    cout<<"Enter first string\n";
+    cin>>str1;
+    cout<<"Enter second string\n";
+    cin>>str2;
+    str3=str1+" "+str2;
+    cout<<"Concatenated string is "<<str3;
+    return 0;
+}
+
+// OUTPUT
+// Enter first string
+// Ayush
+// Enter second string
+// Yadav
+// Concatenated string is Ayush Yadav
