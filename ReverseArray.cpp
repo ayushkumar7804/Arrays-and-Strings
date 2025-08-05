906a2a4e3b5c61f03746ee85baa5169dc2bc6edf
ayushkumar7804 <ayushkumaryadav2029@gmail.com>
2025-08-06 00:16:58 +0530

    Create ReverseArray.cpp

---
+++ ReverseArray.cpp
@@ -0,0 +1,38 @@
+// Name:- Ayush Yadav
+// PRN:- 24070123028
+
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+    
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    
+    reverseArray(arr, size);
+    
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    
+    return 0;
+}
